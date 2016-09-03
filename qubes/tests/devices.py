668a7b616f3844fa71a871c6f6a6e4a625b2f11d
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2016-09-03 02:10:59 +0200

    qubes: new devices API
    Allow device plugin to list attached and available devices. Enforce
    at API level every device being exposed by some domain.
    
    This commit only changes devices API, but not update existing users
    (pci) yet.
    
    QubesOS/qubes-issues#2257

--- qubes/tests/devices.py
+++ qubes/tests/devices.py
@@ -30,32 +30,51 @@ import qubes.tests
 class TestDevice(qubes.devices.DeviceInfo):
     pass
 
+
+class TestVMCollection(dict):
+    def __iter__(self):
+        return iter(set(self.values()))
+
+
 class TestApp(object):
     def __init__(self):
-        self.domains = {}
+        self.domains = TestVMCollection()
+
 
 class TestVM(qubes.tests.TestEmitter):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, app, name, *args, **kwargs):
         super(TestVM, self).__init__(*args, **kwargs)
-        self.app = TestApp()
-        self.app.domains = {
-            'vm': self,
-            self: self,
-        }
-        self.device = TestDevice(self, 'testdev')
+        self.app = app
+        self.name = name
+        self.device = TestDevice(self, 'testdev', 'Description')
         self.events_enabled = True
+        self.devices = {
+            'testclass': qubes.devices.DeviceCollection(self, 'testclass')
+        }
+        self.app.domains[name] = self
+        self.app.domains[self] = self
+
+    def __str__(self):
+        return self.name
 
     @qubes.events.handler('device-list-attached:testclass')
-    def dev_testclass_list_attached(self, event, persistent, attached):
-        if self.device.frontend_domain and attached is not False:
-            yield self.device
+    def dev_testclass_list_attached(self, event, persistent):
+        for vm in self.app.domains:
+            if vm.device.frontend_domain == self:
+                yield vm.device
+
+    @qubes.events.handler('device-list:testclass')
+    def dev_testclass_list(self, event):
+        yield self.device
+
 
 class TC_00_DeviceCollection(qubes.tests.QubesTestCase):
     def setUp(self):
-        self.emitter = TestVM()
+        self.app = TestApp()
+        self.emitter = TestVM(self.app, 'vm')
+        self.app.domains['vm'] = self.emitter
         self.device = self.emitter.device
-        self.collection = qubes.devices.DeviceCollection(self.emitter, 'testclass')
-
+        self.collection = self.emitter.devices['testclass']
 
     def test_000_init(self):
         self.assertFalse(self.collection._set)
@@ -95,7 +114,8 @@ class TC_00_DeviceCollection(qubes.tests.QubesTestCase):
 
 class TC_01_DeviceManager(qubes.tests.QubesTestCase):
     def setUp(self):
-        self.emitter = TestVM()
+        self.app = TestApp()
+        self.emitter = TestVM(self.app, 'vm')
         self.manager = qubes.devices.DeviceManager(self.emitter)
 
     def test_000_init(self):
